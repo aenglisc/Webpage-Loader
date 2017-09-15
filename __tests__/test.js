53776dd3854fa2a0d2aa1e259116643433125e53
Roman Pushkov <pushkovroman@me.com>
2017-09-15 14:57:07 +0300

    removed unnecessary tests

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,61 +1,23 @@
 import nock from 'nock';
 import fs from 'mz/fs';
+import os from 'os';
 import path from 'path';
-import loadPage from '../src/loadPage';
-import makeName from '../src/makeName';
-import getPath from '../src/getPath';
-import run from '../';
+import webpageLoader from '../';
 
 const localhost = 'http://localhost';
-const genFolder = () => fs.mkdtempSync('tmp/');
+// const fixturesDir = './__tests__/__fixtures__/';
+const osTempDir = os.tmpdir();
+const genFolder = () => fs.mkdtempSync(osTempDir);
 const createNock = () => nock(localhost)
   .get('/')
   .reply(200, 'Hello world!');
-
-test('make name: short', () => {
-  const address = 'https://hexlet.io/';
-  expect(makeName(address)).toBe('hexlet-io.html');
-});
-
-test('make name: long', () => {
-  const address = 'https://ru.hexlet.io/projects/4/sessions/118';
-  expect(makeName(address)).toBe('ru-hexlet-io-projects-4-sessions-118.html');
-});
-
-test('load page: local', () => {
-  createNock();
-
-  expect.assertions(2);
-  return loadPage(localhost)
-    .then(({ status, data }) => {
-      expect(status).toBe(200);
-      expect(data).toBe('Hello world!');
-    });
-});
-
-test('load page: remote', () => {
-  expect.assertions(1);
-  return loadPage('https://google.com/')
-    .then(({ status }) => {
-      expect(status).toBe(200);
-    });
-});
-
-test('get path: default', () => {
-  expect(getPath('test.html', './tmp')).toBe(path.resolve('./tmp/test.html'));
-});
-
-test('get path: custom', () => {
-  expect(getPath('test.html', '/test')).toBe(path.resolve('/test/test.html'));
-});
 
 test('download file', () => {
   createNock();
   const dir = genFolder();
 
   expect.assertions(1);
-  return run(localhost, dir)
-    .then(() => fs.readFile(getPath('localhost.html', dir), 'utf8'))
+  return webpageLoader(localhost, dir)
+    .then(() => fs.readFile(path.join(dir, 'localhost.html'), 'utf8'))
     .then(data => expect(data.toString()).toBe('Hello world!'));
 });
-
