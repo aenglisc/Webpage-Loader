578d7f4fd3a068160a79bfb058cb8c347458c902
Roman Pushkov <pushkovroman@me.com>
2017-09-15 16:36:39 +0300

    cleaned up

--- __tests__/test.js
+++ __tests__/test.js
@@ -7,14 +7,12 @@ import webpageLoader from '../';
 const localhost = 'http://localhost';
 // const fixturesDir = './__tests__/__fixtures__/';
 const osTempDir = os.tmpdir();
-// const genFolder = () => fs.mkdtempSync(osTempDir);
 const createNock = () => nock(localhost)
   .get('/')
   .reply(200, 'Hello world!');
 
 test('download file', () => {
   createNock();
-  // const dir = genFolder();
 
   expect.assertions(1);
   return webpageLoader(localhost, osTempDir)
