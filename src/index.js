fb53c72351f060f86bc3cfd7da301fbe8d51c994
Roman Pushkov <pushkovroman@me.com>
2017-09-15 15:00:36 +0300

    removed sync functions

--- src/index.js
+++ src/index.js
@@ -1,16 +1,41 @@
 import fs from 'mz/fs';
-import makeName from './makeName';
-import loadPage from './loadPage';
-import getPath from './getPath';
+import path from 'path';
+// import cheerio from 'cheerio';
+import axios from './axios';
+import { makePageName, makeSrcDirName } from './makeName';
 
-export default (address, destination = './tmp') => loadPage(address)
-  .then(({ data }) => {
-    const fileName = makeName(address);
-    const filePath = getPath(fileName, destination);
-    if (!fs.existsSync(destination)) {
-      fs.mkdirSync(destination);
-    }
-    fs.writeFile(filePath, data, 'utf8')
-      .catch(e => console.log(e));
-  })
-  .catch(e => console.log(e));
+/*
+const srcTypes = {
+  link: 'href',
+  img: 'src',
+  script: 'src',
+};
+*/
+
+// const getSrc = ()
+
+
+const mkdir = (exists, destination) => (exists ? undefined : fs.mkdir(destination));
+/*
+const makePath = (address, destination) => {
+  const outputPath = path.resolve(destination);
+  return path.join(outputPath, `${makePageName(address)}`);
+};
+*/
+
+export default (address, destination = './tmp') => {
+  const pageName = makePageName(address);
+  const srcDirName = makeSrcDirName(address);
+
+  const baseDir = path.resolve(destination);
+  const pageLoc = path.resolve(destination, pageName);
+  const srcDir = path.resolve(destination, srcDirName);
+
+  return fs.exists(baseDir)
+    .then(exists => mkdir(exists, baseDir))
+    .then(() => fs.exists(srcDir))
+    .then(exists => mkdir(exists, srcDir))
+    .then(() => axios.get(address))
+    .then(({ data }) => fs.writeFile(pageLoc, data, 'utf8'))
+    .catch(e => e);
+};
