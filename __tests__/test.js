83e19c90f10d2f974a6386ff752ee1d2012309b7
Roman Pushkov <pushkovroman@me.com>
2017-09-16 06:46:16 +0300

    test tweak

--- __tests__/test.js
+++ __tests__/test.js
@@ -5,11 +5,12 @@ import path from 'path';
 import webpageLoader from '../';
 
 const localhost = 'http://localhost';
+const basicHTML = '<html><head></head><body>Hello world!</body></html>';
 // const fixturesDir = './__tests__/__fixtures__/';
 const osTempDir = os.tmpdir();
 const createNock = () => nock(localhost)
   .get('/')
-  .reply(200, 'Hello world!');
+  .reply(200, basicHTML);
 
 test('download file', () => {
   createNock();
@@ -17,5 +18,5 @@ test('download file', () => {
   expect.assertions(1);
   return webpageLoader(localhost, osTempDir)
     .then(() => fs.readFile(path.join(osTempDir, 'localhost.html'), 'utf8'))
-    .then(data => expect(data.toString()).toBe('Hello world!'));
+    .then(data => expect(data.toString()).toBe(basicHTML));
 });
