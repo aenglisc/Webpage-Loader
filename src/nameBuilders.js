171c109883a1d2de2f7f0436e5c69e0927166ac3
Roman Pushkov <pushkovroman@me.com>
2017-09-16 06:47:18 +0300

    renamed naming module, added source support

---
+++ src/nameBuilders.js
@@ -0,0 +1,16 @@
+import url from 'url';
+import path from 'path';
+
+const transform = str => (str[0] === '/' ? str.slice(1) : str).replace(/\W+/g, '-');
+
+const urlToName = (address) => {
+  const { host, pathname } = url.parse(address);
+  return transform(`${!host ? '' : host}${pathname === '/' ? '' : pathname}`);
+};
+
+export const genPageName = address => `${urlToName(address)}.html`;
+export const genSrcDirName = address => `${urlToName(address)}_files`;
+export const genSrcName = (address) => {
+  const { dir, name, ext } = path.parse(address);
+  return `${urlToName(path.join(dir, name))}${ext}`;
+};
