f3355d5fbf0c0d5d43bc3c993ee5ecb20f51d0ab
Roman Pushkov <pushkovroman@me.com>
2017-09-17 17:39:58 +0300

    test tweaks

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,26 +1,34 @@
 import nock from 'nock';
 import fs from 'mz/fs';
 import path from 'path';
-// import rmrf from 'rimraf';
+import rmrf from 'rimraf';
 import webpageLoader from '../';
 
 const localhost = 'http://localhost';
-const fixturesDir = './__tests__/__fixtures__/';
-const tmpDir = './__tests__/__tmp__/';
-fs.mkdirSync(tmpDir);
+const fixturesDir = path.resolve('./__tests__/__fixtures__/');
+const tmpDir = path.resolve('./__tests__/__tmp__/');
 
-const newhtml = fs.readFileSync(`${fixturesDir}result/test.1.html`, 'utf8');
-const html = fs.readFileSync(`${fixturesDir}base/test.html`, 'utf8');
-const icon = fs.readFileSync(`${fixturesDir}base/icon.ico`, 'utf8');
-const img = fs.readFileSync(`${fixturesDir}base/home.png`, 'utf8');
-const script = fs.readFileSync(`${fixturesDir}base/script.js`, 'utf8');
+const newhtmlloc = path.resolve(fixturesDir, 'result/test.1.html');
+const newhtml = fs.readFileSync(newhtmlloc, 'utf8');
 
-const gentmpdir = () => fs.mkdtempSync(tmpDir);
+const htmlloc = path.resolve(fixturesDir, 'base/test.html');
+const html = fs.readFileSync(htmlloc, 'utf8');
+
+const iconloc = path.resolve(fixturesDir, 'base/icon.ico');
+const icon = fs.readFileSync(iconloc, 'utf8');
+
+const imgloc = path.resolve(fixturesDir, 'base/home.png');
+const img = fs.readFileSync(imgloc, 'utf8');
+
+const scriptloc = path.resolve(fixturesDir, 'base/script.js');
+const script = fs.readFileSync(scriptloc, 'utf8');
+
 let tmpdir = '';
 
 describe('test', () => {
-  beforeEach(() => {
-    tmpdir = gentmpdir();
+  beforeAll(async () => {
+    fs.mkdirSync(tmpDir);
+    tmpdir = fs.mkdtempSync(`${tmpDir}/`);
     nock(localhost)
       .get('/')
       .reply(200, html)
@@ -30,38 +38,35 @@ describe('test', () => {
       .reply(200, img)
       .get('/script.js')
       .reply(200, script);
+    await webpageLoader(localhost, tmpdir);
   });
 
   // clean up
-  /*
+
   afterAll(() => {
     rmrf(tmpDir, () => {});
   });
-  */
 
   test('html', async () => {
-    await webpageLoader(localhost, tmpdir);
-    const dlhtml = await fs.readFile(`${tmpdir}/localhost.html`, 'utf8');
+    const dlhtmlloc = path.resolve(tmpdir, 'localhost.html');
+    const dlhtml = await fs.readFile(dlhtmlloc, 'utf8');
     await expect(dlhtml).toBe(newhtml);
   });
 
   test('icon', async () => {
     const tmpdirSrc = path.join(tmpdir, 'localhost_files');
-    await webpageLoader(localhost, tmpdir);
     const dlicon = await fs.readFile(`${tmpdirSrc}/icon.ico`, 'utf8');
     await expect(dlicon).toBe(icon);
   });
 
   test('img', async () => {
     const tmpdirSrc = path.join(tmpdir, 'localhost_files');
-    await webpageLoader(localhost, tmpdir);
     const dlimg = await fs.readFile(`${tmpdirSrc}/home.png`, 'utf8');
     await expect(dlimg).toBe(img);
   });
 
   test('script', async () => {
     const tmpdirSrc = path.join(tmpdir, 'localhost_files');
-    await webpageLoader(localhost, tmpdir);
     const dlscript = await fs.readFile(`${tmpdirSrc}/script.js`, 'utf8');
     await expect(dlscript).toBe(script);
   });
